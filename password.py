e2533bccf255047d0ed3fc98e3c606479bff45da
Yana Yatcik <vseteplee@gmail.com>
2020-11-07 23:43:00 +0300

    two tasks done

--- password.py
+++ password.py
@@ -1,10 +1,8 @@
-"""
-Программа оценивает сложность пароля.
-
-Пользователь вводит пароль, в ответ получает оценку "сложный"/"простой"
-Сложным считается пароль, состоящий как минимум из 8-ми символов,
-включая цифры и алфавитные символы
-"""
-
-if __name__ == '__main__':
-    pass
+password = input('enter password:\n')
+has_number = any(char.isdigit() for char in password)
+has_letter = any(char.isalpha() for char in password)
+if len(password) < 8:
+    print('password is not strong enough')
+elif has_number*has_letter:
+    print('password is strong enough')
+else: print('password is not strong enough')
